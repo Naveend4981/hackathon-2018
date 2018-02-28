c2a3d9de1610615421a106d161a9814201399cb5
D <naveend@hpe.com>
2018-02-28 20:55:00 +0530

    Change in index html file

--- complete/src/main/java/com/hpe/event_optimizer/controller/ApplicatonController.java
+++ complete/src/main/java/com/hpe/event_optimizer/controller/ApplicatonController.java
@@ -2,18 +2,20 @@ package com.hpe.event_optimizer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hpe.event_optimizer.service.EventService;
 import com.hpe.event_optimizer.service.ServiceImpl;
 
 @RestController
+@RequestMapping("/")
 public class ApplicatonController {
 	
 	@Autowired
 	private EventService service;
 	
-	@GetMapping("/test")
+	@GetMapping("test")
 	public String getHello(){
 		return service.getHello();
 		//return "Hello World";
